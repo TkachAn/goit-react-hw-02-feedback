521049726227eb8476c6ffa81a532b3331c740ff
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-08 22:13:50 +0300

    Update feedBack.jsx

--- src/components/feedBack/feedBack.jsx
+++ src/components/feedBack/feedBack.jsx
@@ -2,26 +2,59 @@ import css from './feedBack.module.css';
 import React, { Component } from 'react';
 export default class FeedBack extends Component {
   // console.log('fff');<button type="button">Increment by {step}</button>
-  state = {
-    good: 8,
-    neutral: 2,
-    bad: 1,
+  static defaultProps = {
+    InitGood: 0,
+    InitNeutral: 0,
+    InitBad: 0,
   };
+  state = {
+    good: this.props.InitGood, //0;
+    neutral: this.props.InitNeutral, //0;
+    bad: this.props.InitBad, //0;
+  };
+  handleGood = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+  handleNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+  handleBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
   render() {
-    const { good, neutral, bad } = this.props;
+    const { good, neutral, bad } = this.state;
     console.log('this', this);
     console.log('this.props', this.props);
     console.log('this.state', this.state);
     return (
       <div className={css.feedback}>
         <h2>Please leave feedback</h2>
-        <button type="button">good {good}</button>
-        <button type="button">neutral {neutral}</button>
-        <button type="button">bad {bad}</button>
+        <button type="button" onClick={this.handleGood}>
+          good
+        </button>
+        <button type="button" onClick={this.handleNeutral}>
+          neutral
+        </button>
+        <button type="button" onClick={this.handleBad}>
+          bad
+        </button>
         <h2>Statistic</h2>
-        <p>good: {this.state.good}</p>
-        <p>neutral: {this.state.neutral}</p>
-        <p>bad: {this.state.bad}</p>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
       </div>
     );
   }
