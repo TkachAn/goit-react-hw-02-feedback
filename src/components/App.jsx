fb4275066c290f7d4da7bec9cd680ba0a2ac69cf
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-08 23:43:55 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import css from './App.module.css';
-import Counter from './counter/counter'; //Counter
+//import Counter from './counter/counter'; //Counter
 import FeedBack from './feedBack/feedBack'; //Counter
 export const App = () => {
   return (
     <div>
       <FeedBack />
-      <Counter />
-      <div className={css.main}>React homework template</div>
     </div>
   );
 };
