353cd1a86b1d58d22ba079e32510bdee0a815f20
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-08 23:50:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import css from './App.module.css';
-//import Counter from './counter/counter'; //Counter
 import FeedBack from './feedBack/feedBack'; //Counter
 export const App = () => {
   return (
