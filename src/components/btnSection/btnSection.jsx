754e0911b8203410dad8cae2ebde44d329640cb8
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-14 00:21:39 +0300

    Update btnSection.jsx

--- src/components/btnSection/btnSection.jsx
+++ src/components/btnSection/btnSection.jsx
@@ -1,23 +1,7 @@
-import React, { Component } from 'react';
-import css from './btnSection.module.css';
-export default class FeedBack extends Component {
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div className={css.feedback}>
-        <div className={css.button}>
-          <h2 className={css.h2}>Please leave feedback</h2>
-          <button type="button" onClick={this.handleGood}>
-            good
-          </button>
-          <button type="button" onClick={this.handleNeutral}>
-            neutral
-          </button>
-          <button type="button" onClick={this.handleBad}>
-            bad
-          </button>
-        </div>
-      </div>
-    );
-  }
+export function FeedBackBtn({ name, handle }) {
+  return (
+    <button type="button" onClick={handle}>
+      {name}
+    </button>
+  );
 }
