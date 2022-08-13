725127cf7cc1af795d6cb0045521cd0fd1b5c12f
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-13 11:13:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,6 @@ export const App = () => {
   return (
     <section title="">
       <FeedBack />
-      {/* <Section title="Please leave feedback" /> */}
-      {/* <Statistics good={} neutral={} bad={} total={} positivePercentage={}/> */}
-      {/* <FeedbackOptions options={} onLeaveFeedback={}/> */}
     </section>
   );
 };
