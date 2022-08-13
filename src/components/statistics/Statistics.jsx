68bd8e47e5d6565fcb1b5567b73fb75100e8c37b
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-14 00:22:02 +0300

    Create Statistics.jsx

---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,31 @@
+import css from './Statistics.module.css';
+export default function Statistics({ good, neutral, bad }) {
+  const total = good + neutral + bad;
+  const percent =
+    good + neutral + bad !== 0 ? (good / (good + neutral + bad)) * 100 : 0;
+  return (
+    <>
+      <h2 className={css.h22}>Statistic</h2>
+      <div className={css.statistic}>
+        <p className={css.p}>
+          good: <span className={css.number}>{good}</span>
+        </p>
+        <p className={css.p}>
+          neutral: <span className={css.number}>{neutral}</span>
+        </p>
+        <p className={css.p}>
+          bad: <span className={css.number}>{bad}</span>
+        </p>
+        <p className={css.p}>
+          total: <span className={css.number}>{total}</span>
+        </p>
+      </div>
+      <div className={css.percent}>
+        <p>
+          positive feedback:{' '}
+          <span className={css.number}>{percent.toFixed(1)}%</span>
+        </p>
+      </div>
+    </>
+  );
+}
