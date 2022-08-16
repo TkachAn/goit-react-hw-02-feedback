e7da1ed8b61b4194a50f09f878dc98b08ebf6fcd
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-16 17:30:11 +0300

    Update FeedbackOptions.jsx

--- src/components/feedbackOptions/FeedbackOptions.jsx
+++ src/components/feedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
-export default function FeedbackOptions({ options, onLeaveFeedback }) {
+export default function FeedbackOptions({ options, onLeaveFeedback, name }) {
   return (
     // <div className={css.feedback}>
     <div className={css.button}>
       <div>
-        <button type="button" onClick={onLeaveFeedback}>
+        <button name={name} type="button" onClick={onLeaveFeedback}>
           {options}
         </button>
       </div>
