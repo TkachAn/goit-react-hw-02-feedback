37230038ac9a62f56abeecfcc71752187282e5ca
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-18 11:09:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Statistics from './statistics/Statistics';
 import FeedbackOptions from './feedback/FeedbackOptions';
-import css from './App.module.css';
+import Section from 'components/Section/Section';
+import Container from './container/container';
+import css from './container/container.module.css';
 export class App extends Component {
   state = {
     good: 0,
@@ -33,19 +35,24 @@ export class App extends Component {
       this.countPositiveFeedbackPercentage(this.state).toFixed(1)
     );
     return (
-      <div className={css.feedback}>
-        <section title="Feedback" className={css.hh}>
-          <h2>Please leave feedback</h2>
-          <div className={css.pp}>
-            <FeedbackOptions options="good" onLeaveFeedback={this.handleBtn} />
-            <FeedbackOptions
-              options="neutral"
-              onLeaveFeedback={this.handleBtn}
-            />
-            <FeedbackOptions options="bad" onLeaveFeedback={this.handleBtn} />
+      <Container className={css.feedback}>
+        <Section title="Please leave feedback" className={css.hh}>
+          <div className={css.feed}>
+            {/* <h3>Please leave feedback</h3> */}
+            <div className={css.pp}>
+              <FeedbackOptions
+                options="good"
+                onLeaveFeedback={this.handleBtn}
+              />
+              <FeedbackOptions
+                options="neutral"
+                onLeaveFeedback={this.handleBtn}
+              />
+              <FeedbackOptions options="bad" onLeaveFeedback={this.handleBtn} />
+            </div>
           </div>
-        </section>
-        <section title="Statistics">
+        </Section>
+        <Section title="Statistics">
           {good + neutral + bad > 0 ? (
             <Statistics
               good={good}
@@ -57,8 +64,8 @@ export class App extends Component {
           ) : (
             <div className={css.perc}>There is no feedback</div>
           )}
-        </section>
-      </div>
+        </Section>
+      </Container>
     );
   }
 }
